f782f2706ed32f4424f22f7aacaad5492c11e42f
Joeyjas5963 <74683586+Joeyjas5963@users.noreply.github.com>
2022-12-09 16:02:30 -0500

    Add files via upload

--- Python Code/MK Code.py
+++ Python Code/MK Code.py
@@ -1,8 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from MKOBJECT import Player, Team, Match
 from MKOBJECT import PlayerAll, TeamAll
 import csv
 
+# DATA COLLECTION AND OBJECT CREATION
+
 def collect_table_list(file1):
+    ''' function which reads list of tables in GSC Master
+
+    Arguments:
+        file1 (str): name of csv
+
+    Returns:
+        matches (list): list of match objects
+    '''
+
+    # opens and reads data from csv
     with open(file1, 'r') as infile:
         csv_file = csv.reader(infile, delimiter=',')
 
@@ -11,26 +26,45 @@ def collect_table_list(file1):
             data.append(row)
 
         matches = []
-        count = -1
+        count = 0
+
+        # data collection loop
         for i in range(len(data)):
+
+            # check if table has DONE verification
             if 'DONE' in data[i]:
+
+                # sets individual table to variable
                 match = data[i+1:i+17]
 
                 team1_players = []
                 team2_players = []
 
+                # table reading loop
                 for j in range(len(match)):
+
+                    # chops excess columns off of match
                     match[j] = match[j][0:6]
 
+                    # checks if currently reading a row with scores
                     if 1 < j < 8 or 9 < j:
 
+                        # getting player data
                         name = match[j][0]
                         gp1 = match[j][1]
                         gp2 = match[j][2]
                         gp3 = match[j][3]
+
+                        # checks if currently reading team row
                         if j == 7 or j == 10:
+
                             pen = match[j][4]
                             points = match[j][5]
+
+                            if pen == '':
+                                pen = 0
+
+                            # create team object if it's a team row
                             if j == 7:
                                 team1 = Team(name, gp1, gp2, gp3, pen, points)
 
@@ -39,10 +73,15 @@ def collect_table_list(file1):
 
 
                         else:
+
+                            # gets points and placement for player
                             points = match[j][4]
                             placement = match[j][5]
+
+                            # creates player object from player
                             p = Player(name, gp1, gp2, gp3, points, placement)
 
+                        # adds player objects to team rosters
                         if 1 < j < 7:
                             team1_players.append(p)
                         elif j > 10:
@@ -50,6 +89,8 @@ def collect_table_list(file1):
 
                 count += 1
 
+                # gives player objects team association
+                # adds match this player played in as p.num
                 for p in team1_players:
                     p.team = team1.name
                     p.num = count
@@ -57,26 +98,50 @@ def collect_table_list(file1):
                     p.team = team2.name
                     p.num = count
 
+                # adds match these teams played in as team.num
                 team1.num = count
                 team2.num = count
 
 
+                # adds player rosters to team objects
                 team1.players = team1_players
                 team2.players = team2_players
+                both = team1.players + team2.players
 
 
+                # creates name for match
                 name = file1[:-4].replace('_', ' ') + " Match #" + \
                        str(count) + ': ' + match[1][0]
 
-                match = Match(name, match[0][0], count, team1, team2, team1.score,
-                        team2.score, match[2][5], count)
+                date = match[0][0]
+                dif = match[2][5]
 
+                # creates match with all of the above information
+                match = Match(name, date, count, team1, team2, int(match[8][-1]),
+                              int(match[9][-1]), team1.score, team2.score, both,
+                              dif, count)
+
+                # adds match to list of matches
                 matches.append(match)
 
     return matches
 
 def summarize(matches):
+    ''' composes all objects into ALL versions (total season data)
 
+    Arguments:
+        matches (list): list of match objects created by above function
+
+    Returns:
+        player_alls (list): list of player_all objects, overall season data
+        team_alls (list): list of team_all objects, overall season data
+        p_ids (dict): dictionary with ids for each player,
+                      corresponding to index in player_alls list
+        t_ids (dict): dictionary with ids for each team,
+                      corresponding to index in team_alls list
+    '''
+
+    # initializing variables
     player_alls = []
     player_names = []
     p_ids = {}
@@ -87,12 +152,19 @@ def summarize(matches):
     t_ids = {}
     t_id = -1
 
+    # loops through each match in match list
     for match in matches:
 
+        # creates joint list for all players in match
         both = match.t1.players + match.t2.players
 
+        # loops through each player in combined list
         for player in both:
+
+            # checks if it's the first instance of this player
             if player.name not in player_names:
+
+                # create player_all object for this player
                 p_id += 1
                 p = PlayerAll(player.name, player.team,
                               player.gp1, player.gp2,
@@ -100,47 +172,204 @@ def summarize(matches):
                               player.placement, player.num,
                               p_id)
 
+                # add id to dictionary, and player to total list
+                # saves player name for future updates
                 p_ids[p.name] = p_id
                 player_alls.append(p)
                 player_names.append(player.name)
             else:
+
+                # if player already exists in player_alls
+                # find player all object and update it with new match data
                 idx = player_names.index(player.name)
                 player_alls[idx].update(player)
 
+        # redefines both for new loop, looking at team data now
         both = [match.t1, match.t2]
 
+        # loops for each team
         for team in both:
+
+            # check is team is not already in list
             if team.name not in team_names:
+
+                # creates team_all object
                 t_id += 1
                 t = TeamAll(team.name, team.gp1, team.gp2,
                             team.gp3, team.pen, team.score,
                             team.players, team.num, t_id)
 
+                # adds id to dict, adds team_all to list
+                # saves name for future updates
                 t_ids[t.name] = t_id
                 team_alls.append(t)
                 team_names.append(team.name)
             else:
+
+                # updates team object if already in list
                 idx = team_names.index(team.name)
                 team_alls[idx].update(team)
 
             for team_all in team_alls:
                 team_all.roster_calc()
 
-    p_ids.pop('')
-    t_ids.pop('')
+    # gets rid of blank ids in id list (it happens idk)
+    #p_ids.pop('')
+    #t_ids.pop('')
 
     return player_alls, team_alls, p_ids, t_ids
 
+# ANALYTIC FUNCTIONS
+
+def average(player_alls_D):
+    ''' Finds the average score for each player (idk what to do with subs)
+
+    Arguments:
+        player_alls_D (list): list of player_alls
+
+    '''
+
+    # iterates through each player_alls lists
+    for player_alls in player_alls_D:
+        # iterates through each player
+        for player in player_alls:
+
+            # find the average score of each player and print
+            print(player.name)
+            if '/' not in player.name:
+                avg = sum(player.points) / len(player.points) / 3
+                player.avg = format(avg, '.1f')
+                print(player.avg)
+
+def perfect(matches_D):
+    ''' Searches all matches for "perfect" wars
+
+    Arguments:
+        matches_D: list of each divs matches
+    '''
+
+    # set perfect standing list, and list to store perfects
+    perfect_list = [1, 2, 3, 4, 9]
+    perfect_matches = []
+
+    # iterate through each div
+    for matches in matches_D[1:]:
+        # iterate through each match
+        for match in matches:
+
+            # grab just the placements column of bottom team
+            df = match.df
+            place = list(df.iloc[5:, -1])
+            count = 0
+
+            # iterate through each place
+            for num in place:
+                # if match is not perfect, or empty, break
+                if num in perfect_list or -1 in place:
+                    break
+                else:
+                    count += 1
+                    # if each player looked at, and winners got 10 points, store
+                    if count == 5 and match.t1t == 10:
+                        perfect_matches.append(match)
+                        match.perfect = True
+
+    # print names of all winning matches
+    for i in range(len(perfect_matches)):
+        print(perfect_matches[i].name)
+
+    return perfect_matches
+
+def top_scores(matches_D):
+    ''' Creates dataframe of top scores for season
+
+    Arguments:
+        matches_D: list of each divs matches
+    '''
+
+    # create list to store player objects, and framework for df
+    top = []
+    df_dict = {'Score': [], 'Player': [],
+               'Team': [], 'Div': [], 'Match Num': []}
+
+    # iterate through each div
+    for matches in matches_D[1:]:
+        # iterate through each match
+        for match in matches:
+            # iterate through each player in match
+            for player in match.players:
+                # if they scored more than 130, add to df
+                if player.points >= 130:
+
+                    # adding to top player list, and df
+                    top.append(player)
+                    df_dict['Score'].append(player.points)
+                    df_dict['Player'].append(player.name)
+                    df_dict['Team'].append(player.team)
+                    df_dict['Div'].append(matches_D.index(matches))
+                    df_dict['Match Num'].append(player.num)
+
+    # creating and printing df
+    df_top = pd.DataFrame(df_dict).sort_values('Score', ascending=False)
+    print(df_top.to_string())
+
+def score_dist(matches_D):
+    ''' creates distribution of runner scores
+
+    Arguments:
+        matches_D: list of each divs matches
+    '''
+
+    sns.set()
+
+    # list to store scores
+    scores = []
+
+    # iterate through each div
+    for matches in matches_D[1:]:
+        # iterate through each match
+        for match in matches:
+            # iterate through each player
+            for player in match.players:
+                # if player is not a bagger, add to list
+                if player.points > 12:
+                    scores.append(player.points)
+
+    # crate bins for histogram
+    bins = [13] + list(range(20, 180, 5))
+
+    # plot and show histogram
+    plt.hist(scores, bins=bins)
+    plt.show()
+
 def main():
-    file1 = 'GSC_S1_D1.csv'
-    matches = collect_table_list(file1)
-    player_alls, team_alls, p_ids, t_ids = summarize(matches)
-    print(matches[0])
-    print(p_ids)
-    #print(player_alls[42])
+
+    # NECCESARY FUNCTIONS DO NOT COMMENT
+    divs = list(range(1, 8))
+    matches_D = [0]
+    player_alls_D = [0]
+    team_alls_D = [0]
+    p_ids_D = [0]
+    t_ids_D = [0]
+
+    for div in divs:
+        file = 'GSC_S4_D' + str(div) + '.csv'
+        matches = collect_table_list(file)
+        player_alls, team_alls, p_ids, t_ids = summarize(matches)
+        matches_D.append(matches)
+        player_alls_D.append(player_alls)
+        team_alls_D.append(team_alls)
+        p_ids_D.append(p_ids)
+        t_ids_D.append(t_ids)
+        print(f'Collected D{div}')
+
+    # Commentable functions
+    #average(player_alls)
+    #perfect(matches_D)
+    top_scores(matches_D)
+    #score_dist(matches_D)
 
 
-    p1, p2 = player_alls[72]
-
+    #print(len(matches_D[6][30].players))
 main()
 
--- Python Code/MKOBJECT.py
+++ Python Code/MKOBJECT.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 
+# constants to be used when S5 table bot data is implemented
 COURSES = ['LC', 'MMM', 'MG', 'TF',
            'MC', 'CM', 'DKS', 'WGM',
            'DC', 'KC', 'MT', 'GV',
@@ -15,13 +16,28 @@ COURSES = ['LC', 'MMM', 'MG', 'TF',
 
 POINTS = [15, 12, 10, 8, 6, 4, 3, 2, 1, 0]
 
-
+# ONE INSTANCE of a player (i.e. EmilP in match #7 against XI)
 class Player():
+    ''' object defining a single instance of a player in a given match
+
+    Attributes:
+        name (str): player name
+        gp1/gp2/gp3 (int): score in each gp
+        points (int): total points scored
+        placement (int): placement in war
+        team (str): team affiliation
+        num (int): match this instance of player played in
+        sum (bool): determines if att_clean should be run
+
+
+    '''
 
     def __init__(self, name, gp1, gp2, gp3, points, placement, team='', num=0, sum=False):
 
+        # creates list for att_clean
         check = [gp1, gp2, gp3, points, placement]
 
+        # setting attributes
         self.name = name
         self.gp1 = gp1
         self.gp2 = gp2
@@ -31,6 +47,7 @@ class Player():
         self.team = team
         self.num = num
 
+        # runs att_clean if told to
         if sum == False:
             self.att_clean(check)
 
@@ -39,13 +56,22 @@ class Player():
         return self.name
 
     def att_clean(self, check):
+        ''' cleans data if there are empty spaces
 
+        Arguments:
+            check (list): list of inputs from __init__
+        '''
+
+        # checks if value can be converted to int
         for i in range(len(check)):
             try:
                 check[i] = int(check[i])
+
+            # if value error, change variable to -1
             except ValueError:
                 check[i] = -1
 
+        # resets attributes after cleaning
         self.gp1 = check[0]
         self.gp2 = check[1]
         self.gp3 = check[2]
@@ -53,30 +79,57 @@ class Player():
         self.placement = check[4]
 
     def spread(self):
+        ''' a more in-depth print of player object
+
+        Arguments:
+            None
+        '''
 
         return [self.team, self.name, self.gp1, self.gp2, self.gp3,
                 self.points, self.placement]
 
     def sub_parse(self):
+        ''' WIP Way to split up subs
+
+        :return:
+        '''
 
         if '/' in self.name:
             name1, name2 = self.name.split('/')
 
-
-
-
+# PLAYER THROUGHOUT SEASON (i.e. all of EmilP's score, places, etc.)
 class PlayerAll(Player):
+    ''' object that contains all stats a player has in a given season
 
-    def __init__(self, name, team, gp1, gp2, gp3, points, placement, num, id, sum=True):
-        Player.__init__(self=self, name=name, team=[team], gp1=[gp1],
+    Attributes:
+        name (str): player name
+        team (str): team affiliation
+        gp1/gp2/gp3 (list): list of all scores from respective gps
+        points (list): list of scores from all matches
+        placement (list): list of placements from all matches
+        num (list): list of matches player played in
+        sum (bool): checks to see if att_clean needs to be run
+    '''
+
+    def __init__(self, name, team, gp1, gp2, gp3, points, placement, num, id, sum=True,
+                 avg = 0):
+
+        # creates player object
+        Player.__init__(self=self, name=name, team=team, gp1=[gp1],
                         gp2=[gp2], gp3=[gp3], points=[points],
                         placement=[placement], num=[num], sum=sum)
 
+        # sets player id
         self.id = id
-    def update(self, player):
-        if player.team not in self.team:
-            self.team.append(player.team)
 
+    def update(self, player):
+        ''' updates player_all object with new match info
+
+        Arguments:
+            player (player obj): player object from new match
+        '''
+
+        # adds new stats to player_all object
         self.num.append(player.num)
         self.gp1.append(player.gp1)
         self.gp2.append(player.gp2)
@@ -92,10 +145,22 @@ class PlayerAll(Player):
 
         return self.name + '\n' + t
 
-
+# ONE INSTANCE of a team (i.e. Daisy playing match #7 against XI)
 class Team():
+    ''' object that defines one instance of a team playing
+
+    Attributes:
+        name (str): team name
+        gp1/gp2/gp3 (int): team gp scores
+        pen (int): pens on team
+        score (int): ending score
+        players (list): list of player objects on team
+        num (int): match team played
+    '''
 
     def __init__(self, name, gp1, gp2, gp3, pen, score, players=[], num=0):
+
+        # sets attributes
         self.name = name
         self.gp1 = gp1
         self.gp2 = gp2
@@ -115,18 +180,38 @@ class Team():
 
         return self.name + " Players: " + string
 
-
+# TEAM THROUGHOUT SEASON (i.e. Daisy overall scores, t-points, etc.)
 class TeamAll(Team):
+    ''' object that contains total season data for team
 
-    def __init__(self, name, gp1, gp2, gp3, pen, score, players, num, id, roster=[]):
+    Attributes:
+        name (str): team name
+        gp1/gp2/gp3 (int): team gp scores
+        pen (int): pens on team
+        score (int): ending score
+        players (list): list of player objects on team
+        num (int): match team played
+        roster (dict): number of appearances per player
+    '''
 
+    def __init__(self, name, gp1, gp2, gp3, pen, score, players, num, id, roster={}):
+
+        # creates team object using inputs
         Team.__init__(self, name=name, gp1=[gp1], gp2=[gp2],
                       gp3=[gp3], pen=[pen], score=[score],
                       players=[players], num=[num])
+
+        # sets team id
         self.id = id
 
     def update(self, team):
+        ''' updates team_all object with new team data
 
+        Arguments:
+            team (team object): team data to be added to team_all
+        '''
+
+        # add new inputs to team_all lists
         self.num.append(team.num)
         self.gp1.append(team.gp1)
         self.gp2.append(team.gp2)
@@ -134,18 +219,69 @@ class TeamAll(Team):
         self.score.append(team.score)
         self.players.append(team.players)
 
-
     def roster_calc(self):
+        ''' Creates roster dictionary
 
+        Arguments:
+            None
+        '''
+
+        # assigns defaultdict to roster
         roster = defaultdict(lambda: 0)
 
+        # counts player appearances in matches
         for group in self.players:
             for player in group:
                 roster[player.name] += 1
 
+        self.roster = roster
+
+        # creates dataframe of player appearances
         df = pd.Series(dict(roster)).sort_values(ascending=False)
 
+        return df
 
+# A match with team and player objects, same as spreadsheet
+class Match():
+
+    def __init__(self, name, date, num, t1, t2, t1t, t2t, t1p, t2p, players, dif, id, df=[],
+                 perfect=False):
+
+        self.name = name
+        self.date = date
+        self.num = num
+        self.t1 = t1
+        self.t2 = t2
+        self.t1t = t1t
+        self.t2t = t2t
+        self.t1p = t1p
+        self.t2p = t2p
+        self.players = players
+        self.dif = dif
+        self.id = id
+        self.df = self.match_df()
+
+    def __repr__(self):
+
+        print(self.name + ' ' + self.date)
+        print(self.match_df())
+
+        return '\n'
+
+    def match_df(self):
+
+        columns = ['Team', 'Player', 'GP1', 'GP2', 'GP3', 'Total', 'Placement']
+        full = []
+        for player in self.t1.players:
+            full.append(player.spread())
+        for player in self.t2.players:
+            full.append(player.spread())
+
+        df = pd.DataFrame(full, columns=columns)
+
+        return df
+
+# DEPENDENT ON S5
 class Race():
 
     def __init__(self, number, course, first, second, third, fourth, fifth,
@@ -163,38 +299,3 @@ class Race():
         self.ninth = ninth
         self.tenth = tenth
 
-
-class Match():
-
-    def __init__(self, name, date, num, t1, t2, t1p, t2p, dif, id, df=[]):
-
-        self.name = name
-        self.date = date
-        self.num = num
-        self.t1 = t1
-        self.t2 = t2
-        self.t1p = t1p
-        self.t2p = t2p
-        self.dif = dif
-        self.id = id
-        self.df = self.match_df()
-
-    def __repr__(self):
-
-        print(self.name)
-        print(self.match_df())
-
-        return 'Date Played: ' + self.date
-
-    def match_df(self):
-
-        columns = ['Team', 'Player', 'GP1', 'GP2', 'GP3', 'Total', 'Placement']
-        full = []
-        for player in self.t1.players:
-            full.append(player.spread())
-        for player in self.t2.players:
-            full.append(player.spread())
-
-        df = pd.DataFrame(full, columns=columns)
-
-        return df
